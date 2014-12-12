a7cc4707a188b18825a7f2389755a239e14edbea
Jeff Moss <jeff.moss@octanner.com>
2014-12-11 19:43:46 -0700

    Adding #cache_key support

--- lib/representable/cache/key.rb
+++ lib/representable/cache/key.rb
@@ -2,7 +2,9 @@ module Representable
   module Cache
     class Key < String
       def initialize(params)
-        if params.respond_to?(:keys)
+        if params.respond_to?(:cache_key)
+          super "[#{params.cache_key}]"
+        elsif params.respond_to?(:keys)
           super params.sort_by { |k,_| k.to_s }.map { |k,v| "#{k}=#{self.class.new(v)}" }.join('&')
         elsif params.respond_to?(:map)
           super params.map { |p| self.class.new(p) }.join('&') 
--- spec/representable/cache_key_spec.rb
+++ spec/representable/cache_key_spec.rb
@@ -3,10 +3,17 @@ require 'representable'
 require 'representable/cache/key'
 
 describe Representable::Cache::Key do
+  context "objects responding to #cache_key" do
+    let(:an_object) { double('an object', cache_key: 'a_cache_key') }
+    let(:subject) { described_class.new(an_object) }
+
+    it { should eq "[a_cache_key]" }
+  end
+
   context "hash parameters" do
     let(:subject) { described_class.new('one'=>1,'two'=>2,'three'=>3) }
 
-    it { puts subject; should eq 'one=[1]&three=[3]&two=[2]' }
+    it { should eq 'one=[1]&three=[3]&two=[2]' }
   end
   
   context "array parameters" do
